0a533b462ad2efc4f13ad64e14072d499822482f
karthik-119 <30727836+karthik-119@users.noreply.github.com>
2017-08-28 23:09:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,11 +4,11 @@ var img=document.getElementById('kar');
 var marginLeft = 0;
 function moveRight()
 {
-    marginLeft = marginLeft+10;
+    marginLeft = marginLeft+5;
     img.style.marginLeft = marginLeft+'px';
 }
 img.onclick=function()
 {
-    var interval=setInterval(moveRight,100);
+    var interval=setInterval(moveRight,50);
 
 };
