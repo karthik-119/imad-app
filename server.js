429481faaefba3124f12dee057725f7e74515cb8
karthik-119 <30727836+karthik-119@users.noreply.github.com>
2017-08-30 16:35:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,11 +15,10 @@ var app = express();
 app.use(morgan('combined'));
 var pool = new Pool(config);
 app.get('/test-db', function (req, res) {
-  pool.query('SELECT * FROM test',function(err,result)
-  {
+  pool.query('SELECT * FROM test',function(err,result){
       if(err)
       {
-      res.status(500),send(err.toString());
+      alert('error');
       }else{
       res.send(JSON.stringify(result.rows));
       }
