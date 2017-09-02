318a2955a4176e4cf8f6bd449867f4c91c63cbfa
karthik-119 <30727836+karthik-119@users.noreply.github.com>
2017-09-02 14:12:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,12 +16,12 @@ var app = express();
 app.use(morgan('combined'));
 
 
-app.get('/test', function (req, res) {
+app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 var pool = new Pool(config);
-app.get('/', function (req, res) {
+app.get('/test', function (req, res) {
   pool.query('SELECT * FROM test',function(err,result){
       if(err)
       {
