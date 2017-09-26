a533dea924a101982866196d7da3fc03fafcf495
karthik-119 <30727836+karthik-119@users.noreply.github.com>
2017-09-26 17:54:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,18 +1,18 @@
 console.log('Loaded!');
 var submit = document.getElementById('submit_btn');
-submit.onClick=function(){
+submit.onClick = function () {
  var request = new XMLHttpRequest();
  request.onreadystatechange = function(){
 
-    if(request.readyState === XMLHttpRequest.DONE)
-    {
+    if(request.readyState === XMLHttpRequest.DONE){
        if(request.status === 200){
           alert('logged in successfully');     
-       }else if(request.status === 403)
+       }else if(request.status === 403){
           alert('username/password is incorrect');
        }else if(request.status === 500){
           alert('Something went wrong on server');
        }
+      }
  };
 var username = document.getElementById('username').value;
 var password = document.getElementById('password').value;
