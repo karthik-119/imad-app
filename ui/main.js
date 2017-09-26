cf4bde3fabdae7c135bc22c91c93c823330c36c3
karthik-119 <30727836+karthik-119@users.noreply.github.com>
2017-09-26 18:11:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,7 +17,7 @@ var username = document.getElementById('username').value;
 var password = document.getElementById('password').value;
 console.log(username);
 console.log(password);
-request.open('POST', 'http://karthikrockz83.imad.hasura-app.io/login',true);
+request.open('POST', 'http://karthikrockz83.imad.hasura-app.io/createuser',true);
 request.setRequestHeader('Content-Type', 'application/json');
 request.send(JSON.stringify({username:username, password:password}));
 }
