ebba77f9b5789a5781a3b7201f29de92d54fa1c2
karthik-119 <30727836+karthik-119@users.noreply.github.com>
2017-09-26 15:42:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -104,13 +104,3 @@ app.listen(port, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
 
-
-
-
-// Do not change port, otherwise your app won't run on IMAD servers
-// Use 8080 only for local development if you already have apache running on 80
-
-var port = 80;
-app.listen(port, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
-});
