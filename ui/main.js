a104becb5ba5d84ac58d9ef1c68e09b7ddae7205
karthik-119 <30727836+karthik-119@users.noreply.github.com>
2017-09-28 14:58:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,6 +21,24 @@ request.open('POST', 'http://karthikrockz83.imad.hasura-app.io/login',true);
 request.setRequestHeader('Content-Type', 'application/json');
 request.send(JSON.stringify({username:username, password:password}));
 }
+function sub(){
+ function hash(input, salt)
+{
+ var hashed = crypto.pbkdf25Sync(input, salt, 100000, 512, 'sha512');
+return ["pbkfd25", "100000", salt, hashed.toString('hex')].join($);
+}
+var username = req.body.username;
+  var password = req.body.password;
+  var salt = crypto.RandomBytes(128).toString('hex');
+  var dbstring = hash(password, salt);
+  pool.query('INSERT into "user"(username,password) VALUES ($1,$2)',[username, dbstring], function(err, result){
+       if(err)
+         res.status(500).send(err.toString());
+        else
+           res.send('User successfully created' + username);
+   });  
+   
+}
 var img=document.getElementById('kar');
 var marginLeft = 0;
 function moveRight()
