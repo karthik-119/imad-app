212603c919886836a1699061b96a065be4e5cc7e
karthik-119 <30727836+karthik-119@users.noreply.github.com>
2017-09-28 18:53:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
-var BodyParser = require('body-parser');
+var bodyParser = require('body-parser');
 
 var config ={
 
@@ -16,6 +16,7 @@ var config ={
 };
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser(json));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
