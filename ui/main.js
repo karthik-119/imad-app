fe1804c9dc57e0ddebc07d870920f141ea576cfc
karthik-119 <30727836+karthik-119@users.noreply.github.com>
2017-09-28 16:49:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,26 +1,5 @@
 console.log('Loaded!');
-    document.getElementById("submit").onclick = function() {
- var request = new XMLHttpRequest();
- request.onreadystatechange = function(){
 
-    if(request.readyState === XMLHttpRequest.DONE){
-       if(request.status === 200){
-          alert('logged in successfully');     
-       }else if(request.status === 403){
-          alert('username/password is incorrect');
-       }else if(request.status === 500){
-          alert('Something went wrong on server');
-       }
-      }
- };
-var username = document.getElementById('username').value;
-var password = document.getElementById('password').value;
-console.log(username);
-console.log(password);
-request.open('POST', 'http://karthikrockz83.imad.hasura-app.io/login',true);
-request.setRequestHeader('Content-Type', 'application/json');
-request.send(JSON.stringify({username:username, password:password}));
-};
 var img=document.getElementById('kar');
 var marginLeft = 0;
 function moveRight()
