eca780fd20ae982f729be263aa1ea6247325fdec
karthik-119 <30727836+karthik-119@users.noreply.github.com>
2017-09-28 16:56:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-app.get('/ui/signup.js', function (req, res) {
+app.post('/ui/signup.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'signup.js'));
 });
 
