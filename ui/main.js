44cb1defd123fefcfba946ddf08d77f0bc9a3f66
karthik-119 <30727836+karthik-119@users.noreply.github.com>
2017-09-28 16:37:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 console.log('Loaded!');
-var submit = document.getElementById("submit");
-submit.onclick = function() {
+window.onload=function(){
+    document.getElementById("submit").onclick = function() {
  var request = new XMLHttpRequest();
  request.onreadystatechange = function(){
 
@@ -22,7 +22,7 @@ request.open('POST', 'http://karthikrockz83.imad.hasura-app.io/login',true);
 request.setRequestHeader('Content-Type', 'application/json');
 request.send(JSON.stringify({username:username, password:password}));
 };
-
+};
 var img=document.getElementById('kar');
 var marginLeft = 0;
 function moveRight()
